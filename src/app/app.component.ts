d709a4942b87cdc703fa342cdff6c8d2f0d4a48a
craneo2099 <crankras@hotmail.com>
2019-04-12 02:24:49 -0500

    Checar redimension de pantalla con el teclado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,60 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { AjustesPage } from './../pages/ajustes/ajustes';
+import { AcercaPage } from './../pages/acerca/acerca';
+import { AddDarumaQrPage } from './../pages/add-daruma-qr/add-daruma-qr';
+import { InicioLoginPage } from '../pages/inicio-login/inicio-login';
+import { DarumasGralPage } from './../pages/darumas-gral/darumas-gral';
+
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, App } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild('NAV') nav: Nav;
+  public rootPage:any;
+  public pages: Array<{titulo: string, color: string,
+    componente: any, icon: string}>
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public app: App
+    ) {
+    this.rootPage = InicioLoginPage;
+    this.pages = [
+
+        {titulo: "Mis Darumas", color: "azul", componente: DarumasGralPage, icon: "home"},
+        {titulo: 'Agregar Daruma', color: "azul", componente: AddDarumaQrPage, icon: 'qr-scanner'},
+        {titulo: 'Acerca de', color: "azul", componente: AcercaPage, icon: 'information-circle'},
+        {titulo: "Ajustes", color: "azul", componente: AjustesPage, icon: "settings"},
+        {titulo: "Salir", color: "rosa3", componente: InicioLoginPage, icon: "log-out"}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      if(this.platform.is('android')) {
+        statusBar.styleBlackTranslucent();
+      } else {
+        statusBar.styleDefault();
+      }
+
       splashScreen.hide();
+
     });
+  }
+
+  goToPage(page){
+    //console.log(page);
+    if (page == InicioLoginPage || page == DarumasGralPage) {
+      // Nota: Quita token siempre al cargar inicioPage
+      this.nav.setRoot(page);
+    }else{
+      this.nav.push(page);
+    }
   }
 }
 
